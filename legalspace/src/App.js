9f2b19e7306c0e28f803049ab7e10630a01ade7c
Pablo Belmar Apablaza <39891042+EseTipo@users.noreply.github.com>
2022-04-13 22:05:56 -0400

    segundo commit

--- legalspace/src/App.js
+++ legalspace/src/App.js
@@ -11,11 +11,11 @@ function App() {
         </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://github.com/Esetipazo"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Github Pablo Belmar A.
         </a>
       </header>
     </div>
