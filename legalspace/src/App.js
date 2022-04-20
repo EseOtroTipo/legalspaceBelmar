025567ce811747492a93a838e80e6a49b6f2e891
Pablo Belmar Apablaza <39891042+EseTipo@users.noreply.github.com>
2022-04-20 17:43:07 -0400

    barra de navegacion

--- legalspace/src/App.js
+++ legalspace/src/App.js
@@ -1,9 +1,11 @@
 import logo from './logo.svg';
 import './App.css';
+import NarBar from './components/NavBar';
 
 function App() {
   return (
     <div className="App">
+      <NarBar/>
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
---
+++ legalspace/src/components/NavBar.js
@@ -0,0 +1,11 @@
+const NavBar = () =>{
+    return (
+        <div>
+            <li>Inicio</li>
+            <li>Computadoras</li>
+            <li>Celulares</li>
+            <li>Tablets</li>
+        </div>
+    )
+}
+export default NavBar
