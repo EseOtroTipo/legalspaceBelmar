2aaa398daf5f4a01399b88234d62e1c0d3595f82
Pablo Belmar Apablaza <39891042+EseTipo@users.noreply.github.com>
2022-04-25 21:00:10 -0400

    barra de navegacion con componente

--- legalspace/src/App.js
+++ legalspace/src/App.js
@@ -1,31 +1,18 @@
 import logo from './logo.svg';
 import './App.css';
 import NarBar from './components/NavBar';
+import Header from './components/Header';
 
 function App() {
   return (
     <div className="App">
       <NarBar/>
-
+      <Header/>
       {/* <h1 className="text-3xl font-bold underline">
       Hello world!
     </h1>*/}
       
       
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://github.com/Esetipazo"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Github Pablo Belmar A.
-        </a>
-      </header>
     </div>
   );
 }
---
+++ legalspace/src/components/Header.js
@@ -0,0 +1,19 @@
+const Header = () =>{
+    return (
+        <header className="App-header">
+            <img src={logo} className="App-logo" alt="logo" />
+            <p>
+              Edit <code>src/App.js</code> and save to reload.
+            </p>
+            <a
+              className="App-link"
+              href="https://github.com/Esetipazo"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              Github Pablo Belmar A.
+            </a>
+          </header>
+    )
+}
+export default Header
