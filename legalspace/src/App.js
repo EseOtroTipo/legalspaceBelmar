bfaa525d8bf122f5918a29c70512c39ce78477d6
Pablo Belmar Apablaza <39891042+EseTipo@users.noreply.github.com>
2022-04-29 21:31:38 -0400

    components I

--- legalspace/src/App.js
+++ legalspace/src/App.js
@@ -1,17 +1,21 @@
 import './App.css';
 import NarBar from './components/NavBar';
 import Header from './components/Header';
+import ItemListContainer from './components/ItemListContainer';
 
 function App() {
   return (
     <div className="App">
       <NarBar/>
       <Header/>
+      <ItemListContainer/>
+
+
+
+
       {/* <h1 className="text-3xl font-bold underline">
       Hello world!
     </h1>*/}
-      
-      
     </div>
   );
 }
---
+++ legalspace/src/components/CartWidget.js
@@ -0,0 +1,12 @@
+const CartWidget = () =>{
+    return (
+        <div class="block py-2 pr-4 pl-3 text-white bg-blue-700 rounded md:bg-transparent md:text-blue-700 md:p-0 dark:text-white">
+                <svg class="h-8 w-8 text-red-500"  fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M3 3h2l.4 2M7 13h10l4-8H5.4M7 13L5.4 5M7 13l-2.293 2.293c-.63.63-.184 1.707.707 1.707H17m0 0a2 2 0 100 4 2 2 0 000-4zm-8 2a2 2 0 11-4 0 2 2 0 014 0z"/>
+                </svg>
+        </div>
+        
+
+    )
+}
+export default CartWidget
