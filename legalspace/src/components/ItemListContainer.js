71caca0e4a2b92b2b35c0440c7fabb561e9f6bf9
Pablo Belmar Apablaza <39891042+EseTipo@users.noreply.github.com>
2022-05-04 17:16:28 -0400

    promesas

--- legalspace/src/components/ItemListContainer.js
+++ legalspace/src/components/ItemListContainer.js
@@ -1,9 +1,37 @@
-import React from 'react'
+import React from 'react';
+import { useEffect } from 'react';
 
 const ItemListContainer = () => {
-  return (
-    <div>ItemListContainer</div>
-  )
-}
 
+    const items = ["producto 1", "producto 2", "producto 3"];
+    useEffect(() => {
+      console.log("kashd");
+
+      const promesa = new Promise((res, rej)=>{
+        const rand = Math.random()
+        console.log("numero randon", rand);
+
+        setTimeout(()=>{
+            if(rand>0.5){
+                res('Promesa satisfecha')
+            }else{
+                rej('Rechazada!')
+            }
+        }, 5000)
+      })
+      promesa
+        .then((result)=>{
+            console.log(result)
+        })
+        .catch((err)=>{
+            console.log('la promesa fue rechazada: ', err)
+        })
+        console.log("se termino de ejecutar el useEffct  ")
+    }, [])
+    
+
+  return (
+    <div style={{border: 'solid green 2px'}}>ItemLinhhstContainer</div>
+  )
+} 
 export default ItemListContainer
